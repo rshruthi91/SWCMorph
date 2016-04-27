241b1d9fda47b232366beceeb505e089d30f8e1d
rshruthi91 <rshruthi91@users.noreply.github.com>
2016-04-26 21:28:15 -0400

    Added a whole bunch of parameters and their definitions Added a function to calculate these parameters Added child and parent QVectors for traversal of tree.

--- segment.cpp
+++ segment.cpp
@@ -3,4 +3,30 @@
 Segment::Segment()
 {
     this->CompartmentList.clear();
+    root=false;
+    qDeleteAll(this->children);
+    children.clear();
+    qDeleteAll(this->parents);
+    parents.clear();
+    this->pathlength=0.0;
+    this->euc_length=0.0;
+    this->cylVolume=0.0;
+    this->cylSurface=0.0;
+    this->frstmVolume=0.0;
+    this->frstmSurface=0.0;
+}
+
+void Segment::updateParams() {
+    QMutableLinkedListIterator<Compartment*> iter(this->CompartmentList);
+    iter.toFront();
+    while(iter.hasNext()){
+        Compartment* c = iter.next();
+        this->cylVolume += c->getcylVolume();
+        this->cylSurface += c->getcylSurface();
+        this->frstmSurface += c->getfrstmSurface();
+        this->frstmVolume += c->getfrstmVolume();
+        this->pathlength += c->getlength();
+    }
+    this->euc_length = this->CompartmentList.first()->getStart()->eucldist(
+                this->CompartmentList.last()->getEnd());
 }
--- segment.h
+++ segment.h
@@ -7,10 +7,28 @@ class Segment
 {
 private:
     //Use qdeleteall to avoid memory leaks
-    QLinkedList<Compartment*> CompartmentList;
+    bool root;
+    QVector<Segment*> children;
+    QVector<Segment*> parents;
+//    double pathlength;
+//    double euc_length;
+//    double cylVolume;
+//    double cylSurface;
+//    double frstmVolume;
+//    double frstmSurface;
+    double calc_cylVolume();
+    double calc_cylSurface();
+    double calc_frstmVolume();
+    double calc_frstmSurface();
 public:
     Segment();
-
+    double pathlength;
+    double euc_length;
+    double cylVolume;
+    double cylSurface;
+    double frstmVolume;
+    double frstmSurface;
+    QLinkedList<Compartment*> CompartmentList;
     inline Compartment* getStart(){
         if(CompartmentList.isEmpty()) return NULL;
         else return this->CompartmentList.first();
@@ -22,6 +40,39 @@ public:
     inline void addCompartment(Compartment* comp){
         CompartmentList.append(comp);
     }
+    inline bool isRoot(){return this->root;}
+    inline void setRoot(bool r){ this->root = r;}
+
+    inline QVector<Segment*> getChildren(){
+        return this->children;
+    }
+
+    inline QVector<Segment*> getParents(){
+        return this->parents;
+    }
+
+    inline bool isChild(Segment* s){
+        if(this->children.contains(s))
+            return true;
+        else
+            return false;
+    }
+
+    inline bool isParent(Segment* s){
+        if(this->parents.contains(s))
+            return true;
+        else
+            return false;
+    }
+
+    inline void addChild(Segment* child) {
+        this->children.append(child);
+    }
+    inline void addParent(Segment* par) {
+        this->parents.append(par);
+    }
+
+    void updateParams();
 };
 
 #endif // SEGMENT_H
