51930d86c38991da12de33a9bd02cb35d8ad41d9
rshruthi91 <rshruthi91@users.noreply.github.com>
2016-05-26 14:04:51 -0400

    Added Code that tells about the software and its author. Also added code to write the json file by invoking the required method from tubetree class.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include "common.h"
 #include "tubetree.h"
 
-int main()
+//#define VOL_SERIES
+
+int main(int argc, char *argv[])
 {
+    if(argc<3){
+        qDebug()<< ">>> eXtensible Statistical Engine for Analysis of Tubular Structures <<<" << endl;
+        qDebug()<< ">>>                     Shruthi Raghavan                             <<<" << endl;
+        qDebug()<< "Usage:" << endl;
+        qDebug()<< "To be Done";
+        qDebug()<< ">>>                                                                  <<<" << endl;
+    }
+
     std::string str;
     qDebug() << "Enter the name of swc file with path: " << endl;
     std::getline(std::cin, str);
@@ -13,8 +23,20 @@ int main()
     qDebug() <<"Read the swc file into nodes";
 
     QString outputvtkfile = fi.absolutePath()+"/"+fi.baseName()+".vtk";
+    tubetree.filebasename = fi.baseName();
     if(tubetree.writeVtkPoly(outputvtkfile)) {
         qDebug() << "Wrote VTK file";
-        return EXIT_SUCCESS;
     }else return EXIT_FAILURE;
+
+    QStringList sl = fi.baseName().split("_");
+    int sl_size = sl.size();
+    tubetree.vol_idz = sl[sl_size-1].toInt();
+    tubetree.vol_idy = sl[sl_size-2].toInt();
+    tubetree.vol_idx = sl[sl_size-3].toInt();
+    QString outputparamsfile = fi.absolutePath()+"/"+fi.baseName()+".json";
+    if(tubetree.writeJson(outputparamsfile)) {
+        qDebug() << "Wrote Params file";
+    }else return EXIT_FAILURE;
+
+    return EXIT_SUCCESS;
 }
