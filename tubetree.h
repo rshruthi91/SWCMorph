8c04e4d0766f1764d8f18c27d7161b57cc431830
rshruthi91 <rshruthi91@users.noreply.github.com>
2016-05-26 14:12:42 -0400

    Added variables to hold overall segments and compartmentlist related statistics. Added enum ParamsFormat to define whether to write a Json file or a binary dat file. Declared the new functions added in tubetree.cpp

--- tubetree.h
+++ tubetree.h
@@ -10,10 +10,57 @@ class TubeTree
 private:
     //QVector<Node> NodeList;
     QString infilepath;
+    QFileInfo infileInfo;
     bool populated;
     QString vtk_major_version = "2";
     QString vtk_minor_version = "0";
+    void writeParams(QJsonObject &json);
+
+    double avg_segvolcyl;
+    double min_segvolcyl;
+    double max_segvolcyl;
+
+    double avg_compvolcyl;
+    double min_compvolcyl;
+    double max_compvolcyl;
+
+    double avg_segvolfrstm;
+    double min_segvolfrstm;
+    double max_segvolfrstm;
+
+    double avg_compvolfrstm;
+    double min_compvolfrstm;
+    double max_compvolfrstm;
+    
+    double avg_segsurfcyl;
+    double min_segsurfcyl;
+    double max_segsurfcyl;
+
+    double avg_compsurfcyl;
+    double min_compsurfcyl;
+    double max_compsurfcyl;
+
+    double avg_segsurffrstm;
+    double min_segsurffrstm;
+    double max_segsurffrstm;
+
+    double avg_compsurffrstm;
+    double min_compsurffrstm;
+    double max_compsurffrstm;
+
+    double total_cylvol;
+    double total_frstmvol;
+    double total_cylsurf;
+    double total_frstmsurf;
+
+    bool globalstatsdone;
+    enum ParamsFormat {
+            Json, Binary
+        };
 public:
+    int vol_idx,vol_idy,vol_idz;
+    ParamsFormat Params;
+    QString filebasename;
     //Use qDeleteAll for the following
     QMap<int,Node*> NodeList; //for checking and understanding
     QVector<Compartment*> CompartmentList;
@@ -32,6 +79,10 @@ public:
     bool writeVtkPoly(QString filename);
 
     inline bool isPopulated(){return this->populated;}
+
+    void getSegStats();
+    void getCompStats();
+    bool writeJson(QString filename);
 };
 
 #endif // TUBETREE_H
